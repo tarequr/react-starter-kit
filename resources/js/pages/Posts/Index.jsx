5195146d2d26e8c1da5458d21fbf5ec1a58869af
tarequr <tarequrr8@gmail.com>
2025-04-24 23:43:57 +0600

    chore: create table for posts

--- resources/js/pages/Posts/Index.jsx
+++ resources/js/pages/Posts/Index.jsx
@@ -1,7 +1,7 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,18 +19,26 @@ export default function Posts() {
                 <div className='flex justify-between items-center mb-4'>
                     <h1 className='text-2xl font-bold'>Posts</h1>
 
+                    <Link
+                        href='/posts/create'
+                        className='bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600'
+                    >
+                        Create Post
+                    </Link>
                     <div className='overflow-x-auto'>
                         <table className='w-full table-auto shadow-lg bg-white dark:bg-neutral-800 rounded-lg'>
                             <thead>
                                 <tr className='bg-neutral-50 dark:bg-neutral-700'>
                                     <th className='px-4 py-2'>ID</th>
                                     <th className='px-4 py-2'>Title</th>
+                                    <th className='px-4 py-2'>Content</th>
                                     <th className='px-4 py-2'>Created At</th>
                                     <th className='px-4 py-2'>Updated At</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
+                                    <td className='border px-4 py-2'>1</td>
                                     <td className='border px-4 py-2'>1</td>
                                     <td className='border px-4 py-2'>1</td>
                                     <td className='border px-4 py-2'>1</td>
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,7 +17,9 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <button type="button" className='absolute inset-0 flex items-center justify-center w-full h-full bg-neutral-900/20 dark:bg-neutral-100/20 text-neutral-900/20 dark:text-neutral-100/20'>
+                            <Link href="/posts">Posts</Link>
+                        </button>
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
