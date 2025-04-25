e617870db467c7e3a7a0553658e347d1377ca1d9
tarequr <tarequrr8@gmail.com>
2025-04-25 22:55:48 +0600

    fixed: table issue fixed

--- resources/js/pages/Posts/Index.tsx
+++ resources/js/pages/Posts/Index.tsx
@@ -1,4 +1,3 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
